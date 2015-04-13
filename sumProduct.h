a1e0f66cd69db3d97b4d3935805db1ddc0c5da4d
cheercheong <cheercheong@gmail.com>
2015-04-13 12:34:09 -0400

    Create sumProduct.h

---
+++ sumProduct.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <vector>
+#include <algorithm>
+#define INF 50
+#define H_col 7							 //Column number of the H matrix (n)
+#define H_row 3							 //Row number of the H matrix (n-k)
+#define Hmax_rowdeg 7                    //Maximum row degree of the H matrix
+#define Hmax_coldeg 3                    //Maximum column degree of the H matrix
+#define MAX_cycle 40                     //Maximum iteration number
+#define PI 3.14159265358979322384626433832795
+#define	Clength sizeof(char)*8        
+#define SIMULATION_BLOCK_NUM 200000        //Number of blocks simulated
+
